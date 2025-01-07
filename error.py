58fad73075adfd78abb8a6b9859dceec2cc01d71
rafiahabib <rafiahabib46@gmail.com>
2025-01-07 17:24:08 +0500

    Update error.py

--- error.py
+++ error.py
@@ -1,6 +1,8 @@
-# This code is intended to calculate the average of a list of numbers
+# This code calculates the average of a list of numbers
 numbers = [10, 20, 30, 40, 50]
 total = sum(numbers)
 average = total / len(numbers)
 
-print("The average is:  + 
+# Corrected line: Convert average to a string for concatenation
+print("The average is: " + str(average))
+
